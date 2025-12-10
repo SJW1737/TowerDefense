3433bba903c0d6b82797f25f7454c30ccca9c325
SJW1737 <sic470212@naver.com>
2025-12-10 12:39:53 +0900

    [Feat] 싱글톤, 노드 작성

---
+++ Assets/02.Script/Node/Node.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public int x;
+    public int y;
+    public bool isBlocked;
+
+    public int gCost;
+    public int hCost;
+    public int fCost => gCost + hCost;
+
+    public Node parent;
+
+    public Node(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        isBlocked = false;
+    }
+}
