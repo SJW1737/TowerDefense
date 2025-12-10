d295bcd15f89000e96d1e0b6d4ef8860a1d37a67
SJW1737 <sic470212@naver.com>
2025-12-10 15:32:23 +0900

    [Feat] 그리드 매니저 생성

--- Assets/02.Script/Manager/GridManager.cs
+++ Assets/02.Script/Manager/GridManager.cs
@@ -2,7 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridManager : MonoBehaviour
+public class GridManager : MonoSingleton<GridManager>
 {
+    public int width;
+    public int height;
 
+    public Node[,] grid;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        InitGrid();
+    }
+
+    private void InitGrid()
+    {
+        grid = new Node[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                grid[x, y] = new Node(x, y);
+            }
+        }
+    }
+
+    public Node GetNode(int x, int y) => grid[x, y];
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        // 세로선
+        for (int x = 0; x <= width; x++)
+        {
+            Gizmos.DrawLine(
+                new Vector3(x, 0, 0),
+                new Vector3(x, height, 0)
+            );
+        }
+
+        // 가로선
+        for (int y = 0; y <= height; y++)
+        {
+            Gizmos.DrawLine(
+                new Vector3(0, y, 0),
+                new Vector3(width, y, 0)
+            );
+        }
+    }
 }
