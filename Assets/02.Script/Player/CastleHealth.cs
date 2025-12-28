f6890e474114a777893c9d4efe9da168b874e16b
SJW1737 <sic470212@naver.com>
2025-12-28 19:44:42 +0900

    [Feat] 타워 체력바 UI

--- Assets/02.Script/Player/CastleHealth.cs
+++ Assets/02.Script/Player/CastleHealth.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class CastleHealth : Health
 {
     [SerializeField] private int castleMaxHP = 100;
+    [SerializeField] private CastleHealthBarUI healthBarUI;
 
     private void Start()
     {
         Init(castleMaxHP);
+        healthBarUI.Bind(this);
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+
+        healthBarUI.UpdateUI();
     }
 
     protected override void Die()
---
+++ Assets/02.Script/UI/CastleHealthBarUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CastleHealthBarUI : MonoBehaviour
+{
+    [SerializeField] private Slider hpSlider;
+    [SerializeField] private TextMeshProUGUI hpText;
+
+    private CastleHealth castleHealth;
+
+    public void Bind(CastleHealth health)
+    {
+        castleHealth = health;
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        if (castleHealth == null) return;
+
+        float ratio = (float)castleHealth.CurrentHp / castleHealth.MaxHp;
+        hpSlider.value = ratio;
+        hpText.text = $"{castleHealth.CurrentHp} / {castleHealth.MaxHp}";
+    }
+}
