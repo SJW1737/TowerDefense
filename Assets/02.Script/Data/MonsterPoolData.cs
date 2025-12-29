3b087e1289dd19638abb432f637b2c758cd75f24
SJW1737 <sic470212@naver.com>
2025-12-29 17:51:57 +0900

    [Feat] 오브젝트 풀링 기초

---
+++ Assets/02.Script/Data/MonsterPoolData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MonsterPoolData
+{
+    public MonsterType type;
+    public GameObject prefab;
+    public int poolSize;
+}
