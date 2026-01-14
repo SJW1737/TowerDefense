3f45966dbb60956a577e96ecbdb8fcf2d2584e9a
SJW1737 <sic470212@naver.com>
2026-01-14 16:55:35 +0900

    [Feat] 해상도 별 카메라 조정

--- Assets/02.Script/Camera/CameraController.cs
+++ Assets/02.Script/Camera/CameraController.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Base Reference")]
+    [SerializeField] private float baseAspect = 760f / 1280f;   //기본 해상도 비율
+    [SerializeField] private float baseOrthoSize = 10f;         //기본 카메라 사이즈
+
+    private Camera cam;
+
+    void Awake()
     {
-        
+        cam = GetComponent<Camera>();
+        cam.orthographic = true;
+        Fit();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnValidate()
     {
-        
+        if (cam == null)
+            cam = GetComponent<Camera>();
+        Fit();
+    }
+
+    void Fit()
+    {
+        float currentAspect = (float)Screen.width / Screen.height;
+
+        if (currentAspect < baseAspect)
+        {
+            //더 세로로 긴 화면
+            cam.orthographicSize = baseOrthoSize * (baseAspect / currentAspect);
+        }
+        else
+        {
+            //기준보다 넓은 화면
+            cam.orthographicSize = baseOrthoSize;
+        }
     }
 }
