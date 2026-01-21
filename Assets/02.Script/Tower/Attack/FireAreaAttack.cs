4e76b9e8795ef6bffe5681d1d26fb2dfd728f172
Seungmin7979 <safetmdals79@gmail.com>
2026-01-21 13:12:40 +0900

    [Feat & Refactor] 불 공격 관련 리펙토링 및 불 프리펩 생성 후 불타워 데이터 연결 및 Layer 설정

--- Assets/02.Script/Tower/Attack/FireAreaAttack.cs
+++ Assets/02.Script/Tower/Attack/FireAreaAttack.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,18 +5,58 @@ public class FireAreaAttack : ITickableAttack
 {
     private readonly Tower tower;
     private readonly List<ITowerEffect> effects;
+
     private readonly GameObject firePrefab;
+    private GameObject fireInstance;
+
+    private bool isFireActive = false;
+    private float noTargetTimer;
+    private const float fireOffDelay = 0.3f; // 0.3초 여유
 
     public FireAreaAttack(Tower tower, List<ITowerEffect> effects, GameObject firePrefab)
     {
         this.tower = tower;
         this.effects = effects;
         this.firePrefab = firePrefab;
+
+        if (firePrefab != null)
+        {
+            fireInstance = GameObject.Instantiate(firePrefab, tower.transform.position, Quaternion.identity, tower.transform);
+
+            fireInstance.transform.localScale = Vector3.one * tower.data.range * 2f;
+
+            fireInstance.SetActive(false); // 처음엔 OFF
+        }
     }
 
     public void Tick(float deltaTime)
     {
         Collider2D[] hits = Physics2D.OverlapCircleAll(tower.transform.position, tower.data.range, LayerMask.GetMask("Monster"));
+
+        bool hasTarget = hits.Length > 0;
+
+        if (hasTarget)
+        {
+            noTargetTimer = 0f;
+
+            if (!isFireActive)
+            {
+                isFireActive = true;
+                fireInstance.SetActive(true);
+            }
+        }
+        else
+        {
+            noTargetTimer += deltaTime;
+
+            if (isFireActive && noTargetTimer >= fireOffDelay)
+            {
+                isFireActive = false;
+                fireInstance.SetActive(false);
+            }
+
+            return;
+        }
 
         foreach (var hit in hits)
         {
--- Assets/02.Script/Tower/Effect/AreaDotEffect.cs
+++ Assets/02.Script/Tower/Effect/AreaDotEffect.cs
@@ -1,20 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class AreaDotEffect : ITowerEffect, IUpgradeableEffect
 {
     private readonly Tower tower;
-    private readonly AreaDotCenterType centerType;
-    private readonly float radius;
     private readonly float duration;
     private readonly float damageRatio;
 
     private int upgradeLevel;
 
-    public AreaDotEffect( Tower tower, AreaDotCenterType centerType, float radius, float duration, float damageRatio)
+    public AreaDotEffect( Tower tower, float duration, float damageRatio)
     {
         this.tower = tower;
-        this.centerType = centerType;
-        this.radius = radius;
         this.duration = duration;
         this.damageRatio = damageRatio;
     }
@@ -29,28 +26,20 @@ public class AreaDotEffect : ITowerEffect, IUpgradeableEffect
         tower.StartCoroutine(DoAreaDot(target));
     }
 
-    private System.Collections.IEnumerator DoAreaDot(Monster target)
+    private IEnumerator DoAreaDot(Monster target)
     {
+        if (target == null)
+            yield break;
+
         float time = 0f;
         float tickInterval = tower.data.GetAttackInterval(upgradeLevel);
 
         int baseDamage = tower.GetEffect<DamageEffect>().GetFinalDamage();
-
         int tickDamage = Mathf.RoundToInt(baseDamage * damageRatio);
 
-        while (time < duration)
+        while (time < duration && target != null)
         {
-            Vector2 center = centerType == AreaDotCenterType.Tower ? tower.transform.position : (target != null ? target.transform.position : (Vector2)tower.transform.position);
-
-            Collider2D[] hits = Physics2D.OverlapCircleAll(center, radius, LayerMask.GetMask("Monster"));
-
-            foreach (var hit in hits)
-            {
-                if (hit.TryGetComponent(out Monster monster))
-                {
-                    monster.TakeDamage(tickDamage);
-                }
-            }
+            target.TakeDamage(tickDamage);
 
             yield return new WaitForSeconds(tickInterval);
             time += tickInterval;
